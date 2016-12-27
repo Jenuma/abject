cdcdfeba39e8925f3f4c3b11d01b1367735d91bd
Clifton Roberts <Jenuma@live.com>
2016-12-27 16:32:49 -0600

    :construction: Fetch balance from bank account and display

--- server/controllers/finance-controller.js
+++ server/controllers/finance-controller.js
@@ -1,10 +1,29 @@
 module.exports = function(protected) {
     var router = require("express").Router();
+    var exec = require("child_process").exec;
     
     router.get("/balance", protected, getBalance);
     
     function getBalance(req, res, next) {
+        var cmd = "casperjs ../../mogul.io/mogul.io.js";
         
+        exec(cmd, function(error, stdout, stderr) {
+            var balance = stdout.match(/Value: (\$(?:,?\d{0,3})*\.\d\d)/);
+            
+            if(balance) {
+                res.status(200).send(balance[1]);
+            } else {
+                var err = new Error("Balance could not be retrieved.");
+                err.status = 503;
+                next(err);
+            }
+        });
+        
+        // TODO
+        // -Get last balance amount from db before scraping bank again
+        // -Set up app to scrape bank every 60s
+        // -Write the new balance to db
+        // -Fix script to not generate output file, or find other solution
     }
     
     return router;
---
+++ server/models/balance.js
@@ -0,0 +1,20 @@
+/**
+ * Balance Model
+ * @namespace Models
+ */
+var mongoose = require("mongoose");
+
+/**
+ * The schema for a Balance.
+ * @constructor Balance
+ * @memberOf Models
+ * @param {string} id - A unique identifier for the contact.
+ * @param {string} name - The name of the contact.
+ * @param {string} email - The email address of the contact.
+ * @param {string} number - The phone number of the contact.
+ */
+var BalanceSchema = new mongoose.Schema({
+    amount: Double, default: "0.0"
+});
+
+exports.Balance = mongoose.model("Balance", BalanceSchema, "finance");
